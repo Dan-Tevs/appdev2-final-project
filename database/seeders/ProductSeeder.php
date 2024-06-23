1b083cfb8c85abe2266bdae8149725f85bb50007
Daniel Teves <danielteves@student.laverdad.edu.ph>
2024-06-23 23:57:47 +0800

    first commit

---
+++ database/seeders/ProductSeeder.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\Product;
+
+class ProductSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        // Product::factory(10)->create();
+
+        // Product::create([
+        //     'name' => 'Sample product',
+        //     'description' => 'This is a sample product.',
+        //     'price' => 15.00,
+        //     'quantity' => 25,
+        // ]);
+    }
+}
